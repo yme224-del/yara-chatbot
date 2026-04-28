6841a8c1da0fcbf36cbdf15e78d76032b92bf5e8
Yara <yme224@nyu.edu>
2026-04-28 12:02:37 +0400

    serve chatbot from Render and fix model

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'yara-chatbot.html'));
+});
 
 app.post('/chat', async (req, res) => {
   try {
@@ -17,6 +22,7 @@ app.post('/chat', async (req, res) => {
       },
       body: JSON.stringify(req.body)
     });
+
     const data = await response.json();
     res.json(data);
   } catch (err) {
